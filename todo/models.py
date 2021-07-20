436d8bb923e040ec6e61cde2d5fa52e77e035e90
mostafatakori <mostafatakor@gmail.com>
2021-07-20 17:23:27 +0430

    First commit

---
+++ todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    momo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(blank=True, null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
